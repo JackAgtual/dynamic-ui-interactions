4c70549a287639c6a20ebcfcbe7a53ca2f169cce
Jack Agtual <jaagtual@gmail.com>
2022-12-20 21:41:23 -0800

    Keeping sub menu visible when hovered over

--- drop-down-menu/index.js
+++ drop-down-menu/index.js
@@ -1,10 +1,13 @@
+const hiddenClassName = 'hidden'
+
 const mainMenus = document.querySelectorAll('.main-menu-item');
 mainMenus.forEach(mainMenu => {
-    mainMenu.addEventListener('mouseover', () => {
-        mainMenu.nextElementSibling.classList.remove('hidden')
-    })
+    mainMenu.addEventListener('mouseover', () => mainMenu.nextElementSibling.classList.remove(hiddenClassName))
+    mainMenu.addEventListener('mouseleave', () => mainMenu.nextElementSibling.classList.add(hiddenClassName))
+})
 
-    mainMenu.addEventListener('mouseleave', () => {
-        mainMenu.nextElementSibling.classList.add('hidden')
-    })
+const subMenus = document.querySelectorAll('.sub-menu')
+subMenus.forEach(subMenu => {
+    subMenu.addEventListener('mouseover', () => subMenu.classList.remove(hiddenClassName))
+    subMenu.addEventListener('mouseleave', () => subMenu.classList.add(hiddenClassName))
 })
